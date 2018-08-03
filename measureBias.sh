5b3f5a0a809c21c276cf1fbdbd3383db35ec03f6
tank <chris@chrismisa.com>
2018-08-03 12:09:53 -0700

    Updated formating of output for easier processing

--- measureBias.sh
+++ measureBias.sh
@@ -28,15 +28,17 @@ export CONTAINER_PING="docker run --rm $PING_IMAGE_NAME"
 # Argument sequence is an associative array
 # between file suffixes and argument strings
 declare -A ARG_SEQ=(
-  ["i0.5s16"]="-c 1500 -i 0.5 -s 16"
-  ["i0.5s56"]="-c 1500 -i 0.5 -s 56"
-  ["i0.5s120"]="-c 1500 -i 0.5 -s 120"
-  ["i0.5s504"]="-c 1500 -i 0.5 -s 504"
-  ["i0.5s1472"]="-c 1500 -i 0.5 -s 1472"
+  ["i0.5s16.ping"]="-c 1500 -i 0.5 -s 16"
+  ["i0.5s56.ping"]="-c 1500 -i 0.5 -s 56"
+  ["i0.5s120.ping"]="-c 1500 -i 0.5 -s 120"
+  ["i0.5s504.ping"]="-c 1500 -i 0.5 -s 504"
+  ["i0.5s1472.ping"]="-c 1500 -i 0.5 -s 1472"
 )
 
 # Tag for data directory
 export DATE_TAG=`date +%Y%m%d%H%M%S`
+# File name for metadata
+export META_DATA="Metadata"
 # Sleep for putting time around measurment
 export SLEEP_CMD="sleep 5"
 # Cosmetics
@@ -48,9 +50,9 @@ mkdir $DATE_TAG
 cd $DATE_TAG
 
 # Get some basic meta-data
-echo "uname -a -> $(uname -a)" >> metadata
-echo "docker -v -> $(docker -v)" >> metadata
-echo "sudo lshw -> $(sudo lshw)" >> metadata
+echo "uname -a -> $(uname -a)" >> $META_DATA
+echo "docker -v -> $(docker -v)" >> $META_DATA
+echo "sudo lshw -> $(sudo lshw)" >> $META_DATA
 
 # Run native
 echo "$B Taking native (control) measurments $B"
