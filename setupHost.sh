c2dba2124835ec59ab27ac80bb700920b3b0e62f
tank <chris@chrismisa.com>
2018-08-05 13:19:27 -0700

    updated setup script

--- setupHost.sh
+++ setupHost.sh
@@ -16,12 +16,12 @@ apt-get update
 apt-get install -y libcap-dev libidn2-0-dev nettle-dev
 apt-get install -y docker.io tmux ndppd
 
-# Pull needed containers from docker hub
-docker pull chrismisa/contools:ping
-docker pull docker:stable-dind
-
 # Download and make iputils (which contains our version of ping)
 git clone https://github.com/iputils/iputils.git
 pushd iputils
 make
 popd
+
+# Pull needed containers from docker hub
+docker pull chrismisa/contools:ping
+docker pull chrismisa/contools:dind
