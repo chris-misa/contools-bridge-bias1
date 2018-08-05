a05caa29eabed611fe37c7717559c26e7024765a
Chris Misa <chris@chrismisa.com>
2018-08-04 19:47:00 -0700

    First draft of docker in docker experiment ipv4 only

--- measureBias.sh
+++ measureBias.sh
@@ -11,6 +11,10 @@
 #
 # Run under sudo
 #
+# For the moment ipv6 seems to not be working in
+# this version of docker in docker . . .
+# Starting with ipv4 tests.
+#
 
 # Address to ping to
 export TARGET_IPV4="10.10.1.2"
@@ -27,9 +31,9 @@ export NATIVE_PING_CMD="$(pwd)/iputils/ping"
 export NATIVE_DEV="eno1d1"
 
 # Info for docker in docker container
-export DIND_IMAGE_NAME="docker:stable-dind"
+export DIND_IMAGE_NAME="chrismisa/contools:dind"
 export DIND_CONTAINER_NAME="docker-in-docker"
-export DIND_IPV6_SUBNET="2601:1c0:cb03:1a9d:eeee::/80"
+export DIND_PING_CMD="/iputils/ping"
 
 # Info for ping container
 export PING_IMAGE_NAME="chrismisa/contools:ping"
@@ -46,23 +50,22 @@ export SLEEP_CMD="sleep 5"
 export B="------------"
 
 # Make a directory for results
-# echo $B Gathering system metadata . . . $B
-# mkdir $DATE_TAG
-# cd $DATE_TAG
-# 
-# # Get some basic meta-data
-# echo "uname -a -> $(uname -a)" >> $META_DATA
-# echo "docker -v -> $(docker -v)" >> $META_DATA
-# echo "sudo lshw -> $(sudo lshw)" >> $META_DATA
+echo $B Gathering system metadata . . . $B
+mkdir $DATE_TAG
+cd $DATE_TAG
+
+# Get some basic meta-data
+echo "uname -a -> $(uname -a)" >> $META_DATA
+echo "docker -v -> $(docker -v)" >> $META_DATA
+echo "sudo lshw -> $(sudo lshw)" >> $META_DATA
 
 # Set up containers
-echo $B Spinning up containers $B
+echo $B Spinning up containers . . . $B
 
 # Spin up docker in docker container
-docker run --rm --privileged -d \
+docker run --rm --privileged -itd \
   --name="$DIND_CONTAINER_NAME" \
-  $DIND_IMAGE_NAME \
-  --ipv6 --fixed-cidr-v6="$DIND_IPV6_SUBNET"
+  $DIND_IMAGE_NAME
 
 # Spin up ping container in native docker
 docker run --rm -itd \
@@ -122,8 +125,26 @@ echo "  ping container in docker in docker up with"
 echo "    ipv4: $DIND_PING_IPV4"
 echo "    ipv6: $DIND_PING_IPV6"
 
-# take a break and see if this works. . .
-
 # Run ipv4 measurements
+echo $B Taking IPv4 measurements . . .$B
+
+for i in ${!ARG_SEQ[@]}
+do
+  echo "  native -> target"
+  $SLEEP_CMD
+  $NATIVE_PING_CMD ${ARG_SEQ[$i]} $TARGET_IPV4 > v4_native_to_target_$i
+
+  echo "  container -> target"
+  $SLEEP_CMD
+  docker exec $PING_CONTAINER_NAME ping ${ARG_SEQ[$i]} $TARGET_IPV4 > v4_container_to_target_$i
+
+  echo "  dind -> container"
+  $SLEEP_CMD
+  docker exec $DIND_CONTAINER_NAME $DIND_PING_CMD ${ARG_SEQ[$i]} $TARGET_IPV4 > v4_dind_to_container_$i
+
+  echo "  dind container -> container"
+  $SLEEP_CMD
+  docker exec $DIND_CONTAINER_NAME docker exec $PING_CONTAINER_NAME ping ${ARG_SEQ[$i]} $TARGET_IPV4 > v4_dind_container_to_container_$i
+done
 
 # Run ipv6 measurements
